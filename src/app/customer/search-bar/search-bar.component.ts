0645294a5eca8d98f82baf255ccc1bbd34f503b5
Maclean Pinto <macleanmauricepinto@gmail.com>
2019-09-16 22:54:44 +0530

    Implement geolocation address mapper

--- src/app/customer/search-bar/search-bar.component.ts
+++ src/app/customer/search-bar/search-bar.component.ts
@@ -1,39 +1,33 @@
 
 import { MapsAPILoader } from '@agm/core';
-import { Marker } from '@agm/core/services/google-maps-types';
-import { Component, ElementRef, NgZone, OnInit, ViewChild, Inject } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, ElementRef, NgZone, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 
-let goole: any;
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent implements OnInit, AfterViewInit {
 
   @ViewChild('searchbar', { static: false, read: ElementRef }) searchbar: ElementRef;
   @ViewChild('serviceHelper', { static: false, read: ElementRef }) serviceHelper: ElementRef;
-  placeService;
-  // @ViewChild('searchbar', { static: false }) public searchElementRef: ElementRef;
   map: google.maps.Map;
 
-  constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone, private router: Router, private storage: Storage) { }
+  constructor(private mapsAPILoader: MapsAPILoader, private router: Router, private storage: Storage) { }
 
   public latitude: number;
   public longitude: number;
-  public searchControl: FormControl;
+  public searchControl: FormControl = new FormControl('', [Validators.required]);
 
   ngOnInit() {
     // set google maps defaults
     this.latitude = 39.8282;
     this.longitude = -98.5795;
-
-    // create search FormControl
-    this.searchControl = new FormControl();
-
+  }
+  ngAfterViewInit() {
     // set current position
     this.setCurrentPosition();
   }
@@ -58,7 +52,6 @@ export class SearchBarComponent implements OnInit {
         // set latitude, longitude and zoom
         this.latitude = place.geometry.location.lat();
         this.longitude = place.geometry.location.lng();
-        console.log(place);
         this.storage.set(fieldValue.field, JSON.parse(JSON.stringify(place)));
       });
     });
@@ -71,25 +64,26 @@ export class SearchBarComponent implements OnInit {
         this.latitude = position.coords.latitude;
         this.longitude = position.coords.longitude;
       });
+      this.fetchPlaceDetails();
     }
   }
 
-  private fetchPlaceDetails(location: google.maps.LatLng) {
-    this.placeService = new google.maps.places.PlacesService(this.serviceHelper.nativeElement);
-    let request: google.maps.places.TextSearchRequest = {
-      location: new google.maps.LatLng(this.latitude, this.longitude),
-      query: 'name'
-    }
-    this.placeService.textSearch(request, this.updatePlaceName)
+  private fetchPlaceDetails() {
+    let geoCoderService = new google.maps.Geocoder();
+    let geocoderRequest: google.maps.GeocoderRequest = {
+      location: new google.maps.LatLng(this.latitude, this.longitude)
+    };
+    geoCoderService.geocode(geocoderRequest, this.updatePlaceName.bind(this));
+
   }
 
-  private updatePlaceName(results: google.maps.places.PlaceResult[], status: google.maps.places.PlacesServiceStatus, pagination: google.maps.places.PlaceSearchPagination) {
-    console.log(results);
+  private updatePlaceName(results: google.maps.GeocoderResult[], status: google.maps.GeocoderStatus) {
+    this.searchControl.setValue(results[0].formatted_address);
   }
-  markerDragEnd(m: Marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-    const location = new google.maps.LatLng($event['coords']['lat'], $event['coords']['lng'])
-    this.fetchPlaceDetails(location);
+  markerDragEnd($event: MouseEvent) {
+    this.latitude = $event['coords']['lat'];
+    this.longitude = $event['coords']['lng'];
+    this.fetchPlaceDetails();
   }
 
 }
