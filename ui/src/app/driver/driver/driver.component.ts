69f63d5e184ab0b4215b15c8c2ab8f9a1172f6bc
macleanpinto <macleanmauricepinto@gmail.com>
2019-11-20 17:07:58 +0000

    Driver page

--- ui/src/app/driver/driver/driver.component.ts
+++ ui/src/app/driver/driver/driver.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { LoadingController } from '@ionic/angular';
+import { RideSearchRequest } from 'src/app/models';
 
 @Component({
-  selector: 'app-driver',
-  templateUrl: './driver.component.html',
-  styleUrls: ['./driver.component.scss'],
+    selector: 'app-driver',
+    templateUrl: './driver.component.html',
+    styleUrls: ['./driver.component.scss'],
 })
 export class DriverComponent implements OnInit {
+    request;
+    constructor(private firestore: AngularFirestore, private loadingController: LoadingController) { }
 
-  constructor(private firestore: AngularFirestore, private loadingController: LoadingController) { }
-
-  ngOnInit() {
-    this.loadingController.create({
-      message: 'Finding Rides...',
-    }).then(loading => loading.present());
-    this.firestore.collection('cabSearchRequests').valueChanges().subscribe(res => {
-      console.log(res);
-      this.loadingController.dismiss();
-    });
-  }
+    ngOnInit() {
+        this.loadingController.create({
+            message: 'Finding Rides...',
+        }).then(loading => loading.present());
+        this.firestore.collection('cabSearchRequests').valueChanges().subscribe(res => {
+            console.log(res);
+            this.request = res;
+            this.loadingController.dismiss();
+        });
+    }
 
 }
